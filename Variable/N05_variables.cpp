b2d7e3868332cc77139771d81f1e88ba4bc201a8
yeop <ehdes625@naver.com>
2020-01-21 03:50:59 -0500

    add,float,double

---
+++ Variable/N05_variables.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+	
+	/*
+	// int : 32bit (4bite)
+	int a = 5;
+	int b = 3;
+
+	int hap = a + b;
+	int cha = a - b;
+	int gop = a * b;
+	int mok = a / b;
+	int namuji = a % b;
+
+	printf("%d + %d = %d\n", a, b, hap);
+	printf("%d - %d = %d\n", a, b, cha);
+	printf("%d * %d = %d\n", a, b, gop);
+	printf("%d / %d = %d\n", a, b, mok);
+	printf("%d %% %d = %d\n", a, b, namuji);
+	
+
+	   
+        // int : only integer, float include point
+        float a = 5;
+        float b = 3;
+
+        float hap = a + b;
+        float cha = a - b;
+        float gop = a * b;
+        float mok = a / b;
+       
+
+        printf("%f + %f = %f\n", a, b, hap);
+        printf("%f - %f = %f\n", a, b, cha);
+        printf("%f * %f = %f\n", a, b, gop);
+        printf("%f / %f = %f\n", a, b, mok);
+        */
+
+	//double : 64bit(8bite)
+	double a = 5;
+        double b = 3;
+
+        double hap = a + b;
+        double cha = a - b;
+        double gop = a * b;
+        double mok = a / b;
+
+
+        printf("%f + %f = %f\n", a, b, hap);
+        printf("%f - %f = %f\n", a, b, cha);
+        printf("%f * %f = %f\n", a, b, gop);
+        printf("%f / %f = %f\n", a, b, mok);
+
+	// float : (floating point)
+}
