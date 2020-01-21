28e97c2a46635f3ad12ed6f011b571eb25cd7abe
yeop <ehdes625@naver.com>
2020-01-21 03:20:41 -0500

    variables

---
+++ Variable/N04_variable2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+        int a = 2; //initialization
+        int b = 3;
+
+        printf("%d + %d = %d\n", a, b, a + b);
+
+        // variable name : alphabet, _ , number(no first letter)
+
+
+}
+
