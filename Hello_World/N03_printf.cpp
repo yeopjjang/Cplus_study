8a9db572062d039267089ec5cb86f63d4b7edc5a
yeop <ehdes625@naver.com>
2020-01-21 02:42:04 -0500

    third

---
+++ Hello_World/N03_printf.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	printf("%d + %d = %d\n", 2, 3, 5);
+
+	printf("%f\n", 3.14);
+	printf("%.2f\n", 3.141592);
+
+	printf("%g\n", 3.141592);
+	printf("%.3g\n", 8723984712.12837468723);
+
+	printf("%c %c %c\n", 'a', 'b', 'c');
+
+	printf("%s\n", "Hello");	
+}
