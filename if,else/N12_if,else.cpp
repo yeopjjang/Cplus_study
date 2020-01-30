3cdbf49b4316cffc3364e87794be806943eb310d
yeop <ehdes625@naver.com>
2020-01-30 02:49:06 -0500

    draft

---
+++ if,else/N12_if,else.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+
+	/*true : 1, false : 0
+
+        if (1) {
+                printf("***\n");
+        }
+
+        else {
+                printf("$$$\n");
+
+        }
+	*/
+
+	int n;
+	scanf("%d", &n);
+
+	if (n % 2 == 0) {
+		printf("n is even number\n");
+	}
+
+	else {
+		printf("n is odd number\n");
+	}
+
+	/*
+	int n;
+	scanf("%d" &n);
+
+	if (n % 2) {
+		printf("n is odd number");
+	}
+
+	else {
+		printf("n is even number");
+	}*/
+
+}
+
